9abb07572b640e6cd4593963ebd1a2457e849eaf
CptCornDog <35270085+CptCornDog@users.noreply.github.com>
2018-01-11 13:23:31 +0000

    FGFHJDK

--- Domminos/Main.cpp
+++ Domminos/Main.cpp
@@ -7,11 +7,11 @@
 #include <algorithm>
 #include <cctype>
 #include <fstream>
-
+ 
 using namespace std;
-
+ 
 // FOR ALL THE FUNCTION DECLARATIONS BELOW, YOU MAY USE VECTORS INSTEAD OF LISTS BUT BEAR IN MIND THE DIFFERENCE IN EFFICIENCY MAY REQUIRE YOU TO ALTER THE IMPLEMENTATION A BIT
-
+ 
 /*
 This function reads a file and stores each line in a list of strings, you should use <fstream> to achieve this
 @param  l: the list of strings
@@ -20,157 +20,145 @@ This function reads a file and stores each line in a list of strings, you should
 */
 void readFile(list<string>& l, string fileName)
 {
-	ifstream file(fileName);
-	string line;
-
-
-	while (getline(file, line))
-	{
-		l.push_back(line);
-	}
-
-
-
-
-
+    ifstream file(fileName);
+    string line;
+ 
+ 
+    while (getline(file, line))
+    {
+        l.push_back(line);
+    }
+ 
+ 
+ 
+ 
+ 
 }
 string toLower(string in) //makes string lower case
 {
-	string firstLower;
-	for (int i = 0; i < in.length(); ++i) {
-		firstLower += tolower(in[i]);
-	}
-	return firstLower;
+    string firstLower;
+    for (int i = 0; i < in.length(); ++i) {
+        firstLower += tolower(in[i]);
+    }
+    return firstLower;
 }
-
+ 
 /*
 This function returns true if a specified string is contained in a list
 @param  l: the list of strings
 @param  s: the string to be checked
 @return true if s is a member of l, false otherwise
 */
-
+ 
 bool isIn(list<string> l, string s)
 {
-	for (string word : l)
-	{
-		
-		if (toLower(s) == toLower(word))
-			return true;
-
-
-	}
-	return false;
-
+    for (string word : l)
+    {
+       
+        if (toLower(s) == toLower(word))
+            return true;
+ 
+ 
+    }
+    return false;
+ 
 }
-
-
-
+ 
+ 
+ 
 /*
 This function returns true if the last letter of one string matches the first letter of another
 @param  first:  the string whose first character is to be checked
 @param  last:   the string whose last character to be checked
 @return true if first and last characteras are equal, regardless of case
 */
-
+ 
 bool match(string first, string last)
 {
-
-
-	string firstLower = toLower(first);
-	string lastLower = toLower(last);
-
-	return firstLower[firstLower.length() - 1] == lastLower[0];
-
-
-
+ 
+    if (last.empty()) { return true; }
+    string firstLower = toLower(first);
+    string lastLower = toLower(last);
+ 
+    return firstLower[firstLower.length() - 1] == lastLower[0];
+ 
+ 
+ 
 }
-
-viod turn (string player)
+ 
+void turn (string player, list<string> words, string &lastWord)
 {
-	cout << "Okay " << player << "please enter your element" << endl;
-	string playerWord = "";
-	getline(cin, playerWord);
+    string playerWord = "";
+    while (true)
+    {
+        cout << "Okay " << player << "please enter your element" << endl;
+        getline(cin, playerWord);
+ 
+        bool in = isIn(words, playerWord);
+        bool isMatch = match(playerWord, lastWord);
+ 
+ 
+        if (in  && isMatch)
+        {
+            cout << "Next move" << endl;
+            break;
+        }
+        else
+        {
+            cout << "Letters dont match you losse" << endl << "press q to quite or r to restart" << endl;
+        }
+    }
+ 
+    lastWord = playerWord;
 }
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 //insert program here...
 int main()
 {
-
-	list<string> words;
-	readFile(words, "Elements.txt");
-
-	
-	cout << "Hello player One please enter your name" << endl;
-	string playerOne = "";
-	getline(cin, playerOne);
-
-
-	cout << "Hello player Two please enter your name" << endl;
-	string playerTwo = "";
-	getline(cin, playerTwo);
-
-	bool player1Turn = true;
-	if (player1turn)
-	{
-		Turn(player1);
-	}
-	else
-	{
-		Turn(player2);
-	}
-	cout << "Okay " << playerOne << "please enter your element" << endl;
-	string playerOneWord = "";
-	getline(cin, playerOneWord);
-	for (string word : words )
-	{
-		if (word == playerOneWord)
-
-	}
-
-	
-	
-	bool in = isIn(words, playerOneWord);
-	bool in2 = isIn(words, playerTwoWord);
-	bool isMatch = match(playerOneWord, playerTwoWord);
-	
-
-	if (in && in2 && isMatch)
-	{
-		cout << "Next move" << endl;
-	}
-	else
-	{
-		cout << "Letters dont match you losse" << endl << "press q to quite or r to restart" << endl;
-	}
-	system("pause");
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	
-
-
-	
-
-
-
-
-
-
-	return 0;
+ 
+    list<string> words;
+    readFile(words, "Elements.txt");
+ 
+   
+    cout << "Hello player One please enter your name" << endl;
+    string playerOne = "";
+    getline(cin, playerOne);
+ 
+ 
+    cout << "Hello player Two please enter your name" << endl;
+    string playerTwo = "";
+    getline(cin, playerTwo);
+ 
+ 
+ 
+    bool playerOneTurn = true;
+    string lastWrod;
+    while (true)
+    {
+        if (playerOneTurn)
+        {
+            turn(playerOne, words, lastWrod);
+ 
+        }
+        else
+        {
+            turn(playerTwo, words, lastWrod);
+        }
+        playerOneTurn = !playerOneTurn;
+    }
+ 
+   
+   
+   
+   
+   
+    system("pause");
+ 
+    return 0;
 }
